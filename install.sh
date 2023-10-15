a542284ace356dfa1ed7ff3c86fbbce8ec9e5442
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-10-15 18:22:59 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -f install.sh
+# ===================
+export green='\033[0;32m'
+export NC='\033[0m'
+rm -f /root/fvstore.sh
+clear
+echo ""
+echo ""
+echo -e "$green DOWNLOADING FILE SETUP......$NC"
+sleep 2
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 
+apt update -y 
+apt upgrade -y 
+apt install -y bzip2 gzip coreutils screen curl unzip 
+apt install lolcat -y 
+apt install neofetch -y
+wget https://raw.githubusercontent.com/FadlyNotNot/v1/main/main.sh && chmod +x main.sh && ./main.sh
+rm -f /root/main.sh
