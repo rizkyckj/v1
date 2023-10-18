20cecacd64fc149e8927d28854996a0808319b31
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-10-18 16:15:43 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-REPO="https://raw.githubusercontent.com/rizkyckj/v1/main/"
 red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 clear
 fun_bar() {
@@ -31,17 +30,17 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget ${REPO}menu/menu.zip
+    wget https://raw.githubusercontent.com/rizkyckj/v1/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
-    rm -rf menu
     rm -rf menu.zip
+    rm -rf update.sh
 }
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          SCRIPT STATUS             \e[0m"
+echo -e " \e[1;97;101m            UPDATE SCRIPT               \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
