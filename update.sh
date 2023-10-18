1999522a32b94e9817f10accf135b2ead27b6e41
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-10-18 15:58:06 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,12 +30,12 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/rizkyckj/v1/main/menu/menu.zip
+    wget ${REPO}menu/menu.zip
     unzip menu.zip
-    chmod +x *
+    chmod +x menu/*
     mv menu/* /usr/local/sbin
+    rm -rf menu
     rm -rf menu.zip
-    rm -rf update.sh
 }
 netfilter-persistent
 clear
